e31bc7b4079c48c9fd05fd4df136ddd379d3a0ba
rohityellapu <rohithyellapu24@gmail.com>
2023-02-17 21:25:38 +0530

    edit link styles

--- src/components/Favorites.jsx
+++ src/components/Favorites.jsx
@@ -7,7 +7,7 @@ const apiURL = "https://api.weatherapi.com/v1/current.json?key=ab45924f6c734aa9a
 
 function Favorites() {
     let weathData = JSON.parse(localStorage.getItem('weather'));
-    let [favData, setWeathData] = useState(weathData?.favorites);
+    let [favData, setWeathData] = useState(weathData?.favorites || []);
     let [item, setItem] = useState('');
     const [load, setloading] = useState(false)
     async function getWeather(loc) {
@@ -34,7 +34,7 @@ function Favorites() {
     return (
         <>
             <div className="p-2">
-                <Link to='/search' className='bg-cyan-400 rounded-xl text-xl text-center p-2 my-2 block hover:saturate-200 active:scale-95'>Get weather by Search<img className='h-6 w-6 mx-2 inline' src="https://img.icons8.com/ios-glyphs/256/search.png" alt="" /></Link>
+                <Link to='/search' className='bg-cyan-400 rounded-xl text-xl text-center p-2 m-4 block hover:saturate-200 active:scale-95'>Get weather by Search<img className='h-6 w-6 mx-2 inline' src="https://img.icons8.com/ios-glyphs/256/search.png" alt="" /></Link>
                 <form onSubmit={ handleSubmit } className='flex justify-around items-center p-2'>
                     <input type="text" value={ item } onChange={ (e) => setItem(e.target.value) } placeholder='Add favorites' className='p-3 border-none rounded-lg focus:outline-green-100 ' />
                     <input type="submit" value="Add" className='p-2 px-4 hover:saturate-200 active:scale-95 rounded-md bg-blue-500 m-2' />
@@ -43,7 +43,7 @@ function Favorites() {
                 <div className="favorites flex flex-wrap">
 
                     { favData?.map((fav, i) => (
-                        <FavWeather key={ fav.name + i } fav={ fav } i={ i } />
+                        <Link to={ `/${fav.name}` }>  <FavWeather key={ fav.name + i } fav={ fav } i={ i } /></Link>
 
 
                     )) }
